30b641f4a63b47ea9757ff765d000a23ae766e17
Christoph Jerolimov <jerolimov+git+2013@gmail.com>
2013-07-30 11:24:54 +0200

    Kickoff project.

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,36 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+
+import de.fhkoeln.gm.cui.javahardener.defectdemos.DemoMethodCall;
+import de.fhkoeln.gm.cui.javahardener.out.ClassPrinter;
 
 public class Test {
-	public static void main(String[] args) {
-		System.out.println("Nothing here... :-)");
+	public static void main(String[] args) throws IOException {
+		Class<?> c = DemoMethodCall.class;
+		String className = c.getName();
+		String outFolder = "out";
+		
+		String classFilename = "/" + className.replace('.', '/') + ".class";
+		InputStream is = c.getResourceAsStream(classFilename);
+		
+		
+		ClassReader classReader = new ClassReader(is);
+		ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+		
+		classReader.accept(new ClassPrinter(classWriter), 0);
+		
+		if (!new File(outFolder + classFilename).getParentFile().exists()) {
+			new File(outFolder + classFilename).getParentFile().mkdirs();
+		}
+		OutputStream os = new FileOutputStream(outFolder + classFilename);
+		os.write(classWriter.toByteArray());
+		os.close();
+		
 	}
 }
--- src/test/java/de/fhkoeln/gm/cui/javahardener/defectdemos/DemoMethodCall.java
+++ src/test/java/de/fhkoeln/gm/cui/javahardener/defectdemos/DemoMethodCall.java
@@ -2,13 +2,28 @@ package de.fhkoeln.gm.cui.javahardener.defectdemos;
 
 @SuppressWarnings("null")
 public class DemoMethodCall {
-	public static void crashInStaticFunction() {
-		Object o = null;
-		o.toString();
+	public static void main(String[] args) {
+		System.out.println(DemoMethodCall.crashInStaticFunction());
+		System.out.println(new DemoMethodCall().crashInMethod());
 	}
 	
-	public void crashInMethod() {
+	public static String crashInStaticFunction() {
 		Object o = null;
-		o.toString();
+		return o.toString();
+	}
+	
+	public String crashInMethod() {
+		Object o = null;
+		return o.toString();
+	}
+
+	@SuppressWarnings("unused")
+	public String checkNull() {
+		Object o = null;
+		if (o != null) {
+			return o.toString();
+		} else {
+			return null;
+		}
 	}
 }
